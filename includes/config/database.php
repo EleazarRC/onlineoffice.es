8b3058817237025c4b0c0b32ad87a0496e2aca49
Eleazar Ramos Cortés <sixen25@gmail.com>
2022-01-07 21:25:33 +0100

    Update database.php

--- includes/config/database.php
+++ includes/config/database.php
@@ -2,7 +2,6 @@
 
 function conectarDB() : mysqli {
     $db = new mysqli('localhost', 'root', 'root', 'onlineoffice');
-    //$db = new mysqli('db5006137245.hosting-data.io', 'dbu1143391', 'Office-2021-Admin', 'dbs5134450');
 
     if(!$db) {
         echo "Error no se pudo conectar";
