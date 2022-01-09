b8fd00cdcc50f5f61f1962fbb0515a83bb1dc176
Eleazar Ramos Cortés <sixen25@gmail.com>
2022-01-09 10:33:52 +0100

    Administradores + release para git

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,8 @@
 <?php 
 
 function conectarDB() : mysqli {
-    $db = new mysqli('localhost', 'root', 'root', 'onlineoffice');
+    //$db = new mysqli('localhost', 'root', 'root', 'onlineoffice');
+    $db = new mysqli('localhost', 'root', 'root', 'dbs5134450');
 
     if(!$db) {
         echo "Error no se pudo conectar";
