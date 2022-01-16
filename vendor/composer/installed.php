72adb6187fb021eb30cd17efb5d4267ab091b566
Eleazar Ramos Cortés <sixen25@gmail.com>
2022-01-16 19:11:32 +0100

    registro de usuarios

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1852c3fb0755e2acb38fbd9634ab19efbcf4f936',
+        'name' => 'eleazarrc/www',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'eleazarrc/www' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1852c3fb0755e2acb38fbd9634ab19efbcf4f936',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.5.3',
+            'version' => '6.5.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'baeb7cde6b60b1286912690ab0693c7789a31e71',
+            'dev_requirement' => false,
+        ),
+    ),
+);
